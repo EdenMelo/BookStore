bd9696f00aeb517bd1b4b11f2f52dba649900ee5
Melo <menglongde777@outlook.com>
2022-07-08 23:11:37 +0800

    bookstore commit test

--- src/test/CartServiceTest.java
+++ src/test/CartServiceTest.java
@@ -53,7 +53,7 @@ public class CartServiceTest {
     public void testFindOrderByUserId(){
         List<Order> list = cartService.findOrderByUserId("3");
         list.forEach(System.out::println);
-        System.out.println("6");
+        System.out.println("1");
     }
 
     @Test
