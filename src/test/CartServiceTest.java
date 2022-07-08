aa6e0b7de3ba17459b8fc78d205ba766e3f4d504
EdenMelo <1371787713@qq.com>
2022-07-08 23:18:01 +0800

    bookstore commit test2

--- src/test/CartServiceTest.java
+++ src/test/CartServiceTest.java
@@ -53,7 +53,7 @@ public class CartServiceTest {
     public void testFindOrderByUserId(){
         List<Order> list = cartService.findOrderByUserId("3");
         list.forEach(System.out::println);
-        System.out.println("1");
+        System.out.println("2");
     }
 
     @Test
